e31bf054f649b3e9387f769a4c29b091c590dcbe
Saoussen ElAGUEL <elaguels@yahoo.com>
2023-09-01 13:16:32 +0200

    techNotes commit

---
+++ backend/models/User.js
@@ -0,0 +1,8 @@
+const mongoose=require('mongoose')
+const userSchema=mongoose.Schema({
+    username:{type:String,required:true},
+    password:{type:String,required:true},
+    roles:[{type:String,default:"employee"}],
+    active:{type:Boolean,default:true}
+})
+module.exports=mongoose.model('User',userSchema)
